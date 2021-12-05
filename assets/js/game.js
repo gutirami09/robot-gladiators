84a97fc6e323c1670d6f742ba36cc73c5c394870
Roberto Ramirez <ramirez.gutierrez09@gmail.com>
2021-12-05 11:35:46 -0800

    added a promt to include a players name and consle loged it.

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+// functions
+function fight() {
+    window.alert("The fight has begun!");
+};
+
+
+// calling functions
+
+//fight();
