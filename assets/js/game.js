b6c3d799ae1cb4ceca16fb13cf1d8e0aaba6c308
Roberto Ramirez <ramirez.gutierrez09@gmail.com>
2021-12-07 16:58:20 -0800

    added the startGame and endGame functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -94,7 +94,59 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+//function to start a new game
+var startGame = function() {
+  //reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
 
+  for (var i = 0; i < enemyNames.length; i++) {
+    if(playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+
+    //after the loop ends, player is either out of health or enemies to fight, so run endGame function
+    endGame();
+  }
+  //play again
+  startGame();
+}
+
+//function to end the entire game
+var endGame = function() {
+  //if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of" + playerMoney + ".")
+  }
+  else {
+    window.alert("You've lost your robot battle.");
+  }
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    //restart game
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+  }
+}
+
+//start the game when the page loads aka call the function or it wont run
+startGame();
+
+/*
 var shop = function () {
   //ask player if they want to "refill" health, "upgrade attack," or "leave shop" i.e window prompt
 }
@@ -113,3 +165,4 @@ function endGame()
 //window alert that the player has lost if defeated
 //window alert with players total stata
 //window asking player if they would like to play again
+*/
